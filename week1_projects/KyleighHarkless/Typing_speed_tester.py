9fcbff9488495f31979089d29d2f0bddf3761746
Kyleigh <candyrae2006@gmail.com>
2025-09-19 23:35:04 -0500

    Add accuracy function to be used in typing speed

--- week1_projects/KyleighHarkless/Typing_speed_tester.py
+++ week1_projects/KyleighHarkless/Typing_speed_tester.py
@@ -25,14 +25,11 @@ Example input/output
 
 (Optional) Add requirements.txt (if you use any libraries)"""
 def typing_speed_test():
-    # WPM = (characters_typed / 5) / minutes_elapsed
-    # accuracy = (correct_chars / total_chars_typed) * 100
-
     print("Type this sentence as fast as you can!")
 
     prompt1 = "The quick brown fox jumps over the lazy dog."
-    # prompt2 = "Despite the heavy rain, Julia sprinted across the crowded street—dodging cars, " \
-    #             "splashing through puddles, and laughing the entire way."
+    prompt2 = "Despite the heavy rain, Julia sprinted across the crowded street—dodging cars, " \
+                "splashing through puddles, and laughing the entire way."
     
     print(prompt1)
 
@@ -41,10 +38,34 @@ def typing_speed_test():
     end = time.time()
 
     characters_typed = len(typed_response.split())
-
     WPM = (characters_typed / 5) / (end - start)
 
     print(f"Words per minute: {WPM:.2f} seconds")
+    print(f"Your accuracy: {accuracy_checker(prompt1, typed_response):.2f}%")
+
+    # if WPM < 0.25:
+    #     print("You're ready for a harder challenge!")
+    #     print(f"{prompt2}\n---------")
+
+def accuracy_checker(prompt:str, user_input:str):
+    correct_char = 0
+    
+    for i in range(min(len(prompt), len(user_input))):
+        if prompt[i] == user_input[i]:
+            correct_char += 1
+    
+    accuracy = (correct_char / len(prompt)) * 100
+
+    return accuracy
+
+    
+
+
+
+
+
+
+
 
 
 
